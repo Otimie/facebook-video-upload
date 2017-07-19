bd8b1c315f7ac7d66c3d09b91cdc4d9208981111
lachlan Blake <lachlanblake@outlook.com>
2017-07-19 14:14:59 +1000

    Updated records to Records

--- post/index.js
+++ post/index.js
@@ -4,23 +4,23 @@ const formData = require('form-data');
 
 exports.handler = (event, context, callback) => {
 
-	var message = JSON.parse(event.records[0].Sns.Message);
+	var message = JSON.parse(event.Records[0].Sns.Message);
 
 	var form = new formData();
 
 	form.append('access_token', message.access_token);
 	form.append('upload_phase', 'finish');
 	form.append('upload_session_id', message.upload_session_id);
-	
+
 	var request = https.request({
 		method: 'post',
 		host: 'graph-video.facebook.com',
 		path: '/v2.3/me/videos',
 		headers: form.getHeaders()
 	});
-	
+
 	form.pipe(request);
-	
+
 	request.on('response', (response) => {
 		response.on('data', (data) => {
 			callback(null);
