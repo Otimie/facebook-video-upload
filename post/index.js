744d0c0e02e51114ed418a81569835c632388391
Lachlan Blake <lachlan.blake10@gmail.com>
2017-07-28 14:59:51 +1000

    When posting populate title and description fields

--- post/index.js
+++ post/index.js
@@ -11,6 +11,14 @@ exports.handler = (event, context, callback) => {
 	form.append('access_token', message.access_token);
 	form.append('upload_phase', 'finish');
 	form.append('upload_session_id', message.upload_session_id);
+	
+	if (message.description) {
+		form.append('description', message.description);
+	}
+	
+	if (message.title) {
+		form.append('title', message.title);
+	}
 
 	var request = https.request({
 		method: 'post',
