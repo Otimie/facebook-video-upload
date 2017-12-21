94967348a59e4bfa6f8ff756adc23e0ee464bf08
Lachlan Blake <lachlan.blake10@gmail.com>
2017-12-21 15:40:19 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const AWS = require('aws-sdk');
 const formData = require('form-data');
 
 const lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
 
 exports.handler = (event, context, callback) => {
 
