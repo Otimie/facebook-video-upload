cce69b99c1cb42f1ab9d02b4939a31189bc8a07e
Lachlan Blake <lachlan.blake10@gmail.com>
2017-12-21 15:49:42 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const formData = require('form-data');
 const lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-
 exports.handler = (event, context, callback) => {
-
+	switch (event.phase) {
+		// Start
+		case (null):
+			start(event, context, callback);
+			break;
+			
+		// Transfer
+		case ('transfer'):
+			transfer(event, context, callback);
+			break;
+			
+		// Finish
+		case ('finish'):
+			finish(event, context, callback);
+			break;
+	}
 };
